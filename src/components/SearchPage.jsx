0b21b65ab4056d7a1bd9f0aa2e0aeb753cdb9307
Sajitha94 <sajithasubramani94@gmail.com>
2025-09-01 14:35:07 +0530

    search bar design

---
+++ src/components/SearchPage.jsx
@@ -0,0 +1,145 @@
+import React from "react";
+import { styled, alpha } from "@mui/material/styles";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import InputBase from "@mui/material/InputBase";
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormHelperText from "@mui/material/FormHelperText";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { useState } from "react";
+
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(1),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  width: "100%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    [theme.breakpoints.up("sm")]: {
+      width: "12ch",
+      "&:focus": {
+        width: "20ch",
+      },
+    },
+  },
+}));
+
+function SearchPage() {
+  const [status, setStatus] = useState("");
+  const [priorities, SetPriorities] = useState("");
+
+  const handleStatusChange = (event) => {
+    setStatus(event.target.value);
+  };
+  const handlePrioritiesChange = (event) => {
+    SetPriorities(event.target.value);
+  };
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-around",
+        marginTop: "30px",
+        alignItems: "center",
+        flexDirection: { xs: "column", sm: "row" },
+        marginLeft: "10px",
+        marginRight: "10px",
+      }}
+    >
+      <Search
+        sx={{
+          width: { sm: "50vw !important" },
+          height: { sm: "9vh" },
+          border: "2px solid #cddc39",
+        }}
+      >
+        <SearchIconWrapper>
+          <SearchIcon />
+        </SearchIconWrapper>
+        <StyledInputBase
+          placeholder="Search…"
+          inputProps={{ "aria-label": "search" }}
+        />
+      </Search>
+
+      <Box>
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <Select
+            value={status}
+            onChange={handleStatusChange}
+            displayEmpty
+            inputProps={{ "aria-label": "Without label" }}
+            sx={{
+              height: { xs: "7vh !important", sm: "9vh !important" },
+              border: "2px solid #cddc39",
+              "& fieldset": { border: "none" },
+            }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={"todo"}>To Do</MenuItem>
+            <MenuItem value={"process"}>In Progress</MenuItem>
+            <MenuItem value={"done"}>Done</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl sx={{ m: 1, minWidth: 120 }}>
+          <Select
+            value={priorities}
+            onChange={handlePrioritiesChange}
+            displayEmpty
+            inputProps={{ "aria-label": "Without label" }}
+            sx={{
+              height: { xs: "7vh  !important", sm: "9vh !important" },
+              border: "2px solid #cddc39",
+              "& fieldset": { border: "none" },
+            }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value={"low"}>Low</MenuItem>
+            <MenuItem value={"medium"}>Medium</MenuItem>
+            <MenuItem value={"high"}>High</MenuItem>
+          </Select>
+        </FormControl>
+      </Box>
+    </Box>
+  );
+}
+
+export default SearchPage;
