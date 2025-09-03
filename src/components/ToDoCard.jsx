fe13ce415a215cbc60793fef508a0ed23eb9a525
Sajitha94 <sajithasubramani94@gmail.com>
2025-09-03 20:52:09 +0530

    all card functions added

--- src/components/ToDoCard.jsx
+++ src/components/ToDoCard.jsx
@@ -230,97 +230,132 @@ function ToDoCard() {
             fontSize: "20px",
           }}
         >
-          To Do
+          In Process
         </Typography>
-        <Card
-          sx={{
-            width: 275,
-            height: "350px",
-            borderLeft: "5px solid #00796b",
-            borderRadius: "5px",
-          }}
-        >
-          <CardContent
-            sx={{
-              height: "240px",
-              overflowY: "auto",
-              pr: 1,
-              scrollbarWidth: "none",
-            }}
-          >
-            <Typography
-              gutterBottom
-              sx={{
-                fontWeight: "bold",
-                wordBreak: "break-word",
-                overflowWrap: "anywhere",
-              }}
-            >
-              Sajitha
-            </Typography>
-
-            <Typography
-              sx={{
-                color: "text.secondary",
-                mb: 1.5,
-              }}
-            >
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo
-              aliquam voluptatem inventore magni perferendis sequi beatae
-              debitis consectetur voluptas exercitationem quos, voluptate
-              possimus blanditiis hic? Cumque labore illo omnis? Vitae?
-            </Typography>
-          </CardContent>
-
-          <CardActions
-            className="flex flex-col gap-4 "
-            sx={{
-              padding: "8px  16px 10px 16px",
-              color: "#616161",
-              alignItems: "start",
-            }}
-          >
-            <Stack direction="row" spacing={1}>
-              <Chip label="Low priorities" />
-              <Chip
-                label="abx"
-                icon={<LocalOfferIcon sx={{ width: "15px" }} />}
-              />
-            </Stack>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                gap: "40px",
-                margin: 0,
-              }}
-            >
-              <Box
+        {filteredBoards.map((card, idx) => {
+          if (card.status === "process") {
+            return (
+              <Card
                 sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "start",
+                  width: 275,
+                  height: "350px",
+                  borderLeft: "5px solid #00796b",
+                  borderRadius: "5px",
                 }}
+                key={idx}
               >
-                <Typography sx={{ fontSize: "16px" }}>updated</Typography>
-                <Typography sx={{ fontSize: "14px" }}>
-                  1/9/2012, 1:22:40pm{" "}
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-around",
-                  gap: "10px",
-                }}
-              >
-                <EditIcon />
-                <DeleteIcon />
-              </Box>
-            </Box>
-          </CardActions>
-        </Card>
+                <CardContent
+                  sx={{
+                    height: "240px",
+                    overflowY: "auto",
+                    pr: 1,
+                    scrollbarWidth: "none",
+                  }}
+                >
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Typography
+                      gutterBottom
+                      sx={{
+                        fontWeight: "bold",
+                        wordBreak: "break-word",
+                        overflowWrap: "anywhere",
+                      }}
+                    >
+                      {card.title}
+                    </Typography>
+                    <LaunchIcon
+                      className="text-gray-600 "
+                      onClick={() => viewCard(card.id)}
+                    />
+                  </Box>
+                  <Typography
+                    sx={{
+                      color: "text.secondary",
+                      mb: 1.5,
+                    }}
+                  >
+                    {card.description}
+                  </Typography>
+                  <Stack direction="row" spacing={1} flexWrap="wrap" useFlexGap>
+                    <Chip
+                      label={card.priority}
+                      sx={{
+                        backgroundColor: "#e0f2f1",
+                        border: "1px solid #ccc",
+                        color: "#424242",
+                        fontWeight: "bold",
+                        fontSize: "13px",
+                        padding: "4px",
+                      }}
+                    />
+                    {card.tags.map((item, idx) => (
+                      <Chip
+                        key={idx}
+                        label={item}
+                        icon={<LocalOfferIcon sx={{ width: "13px" }} />}
+                        sx={{
+                          backgroundColor: "white",
+                          border: "1px solid #ccc",
+                          color: "#616161",
+                          fontWeight: "bold",
+                          fontSize: "11px",
+                        }}
+                      />
+                    ))}
+                  </Stack>
+                </CardContent>
+
+                <CardActions
+                  className="flex flex-col gap-4 "
+                  sx={{
+                    padding: "8px  16px 10px 16px",
+                    color: "#616161",
+                    alignItems: "start",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      gap: "40px",
+                      margin: 0,
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "start",
+                      }}
+                    >
+                      <Typography sx={{ fontSize: "16px" }}>updated</Typography>
+                      <Typography sx={{ fontSize: "14px" }}>
+                        {card.date}{" "}
+                      </Typography>
+                    </Box>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-around",
+                        gap: "10px",
+                      }}
+                    >
+                      <EditIcon
+                        onClick={() => ButtonOnclick("edit", card.id)}
+                      />
+                      <DeleteIcon
+                        onClick={() => ButtonOnclick("delete", card.id)}
+                      />
+                    </Box>
+                  </Box>
+                </CardActions>
+              </Card>
+            );
+          }
+        })}
       </Box>
       <Box
         sx={{
@@ -340,97 +375,132 @@ function ToDoCard() {
             fontSize: "20px",
           }}
         >
-          To Do
+          Done
         </Typography>
-        <Card
-          sx={{
-            width: 275,
-            height: "350px",
-            borderLeft: "5px solid #00796b",
-            borderRadius: "5px",
-          }}
-        >
-          <CardContent
-            sx={{
-              height: "240px",
-              overflowY: "auto",
-              pr: 1,
-              scrollbarWidth: "none",
-            }}
-          >
-            <Typography
-              gutterBottom
-              sx={{
-                fontWeight: "bold",
-                wordBreak: "break-word",
-                overflowWrap: "anywhere",
-              }}
-            >
-              Sajitha
-            </Typography>
-
-            <Typography
-              sx={{
-                color: "text.secondary",
-                mb: 1.5,
-              }}
-            >
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo
-              aliquam voluptatem inventore magni perferendis sequi beatae
-              debitis consectetur voluptas exercitationem quos, voluptate
-              possimus blanditiis hic? Cumque labore illo omnis? Vitae?
-            </Typography>
-          </CardContent>
-
-          <CardActions
-            className="flex flex-col gap-4 "
-            sx={{
-              padding: "8px  16px 10px 16px",
-              color: "#616161",
-              alignItems: "start",
-            }}
-          >
-            <Stack direction="row" spacing={1}>
-              <Chip label="Low priorities" />
-              <Chip
-                label="abx"
-                icon={<LocalOfferIcon sx={{ width: "15px" }} />}
-              />
-            </Stack>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                gap: "40px",
-                margin: 0,
-              }}
-            >
-              <Box
+        {filteredBoards.map((card, idx) => {
+          if (card.status === "done") {
+            return (
+              <Card
                 sx={{
-                  display: "flex",
-                  flexDirection: "column",
-                  alignItems: "start",
+                  width: 275,
+                  height: "350px",
+                  borderLeft: "5px solid #00796b",
+                  borderRadius: "5px",
                 }}
+                key={idx}
               >
-                <Typography sx={{ fontSize: "16px" }}>updated</Typography>
-                <Typography sx={{ fontSize: "14px" }}>
-                  1/9/2012, 1:22:40pm{" "}
-                </Typography>
-              </Box>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "space-around",
-                  gap: "10px",
-                }}
-              >
-                <EditIcon />
-                <DeleteIcon />
-              </Box>
-            </Box>
-          </CardActions>
-        </Card>
+                <CardContent
+                  sx={{
+                    height: "240px",
+                    overflowY: "auto",
+                    pr: 1,
+                    scrollbarWidth: "none",
+                  }}
+                >
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Typography
+                      gutterBottom
+                      sx={{
+                        fontWeight: "bold",
+                        wordBreak: "break-word",
+                        overflowWrap: "anywhere",
+                      }}
+                    >
+                      {card.title}
+                    </Typography>
+                    <LaunchIcon
+                      className="text-gray-600 "
+                      onClick={() => viewCard(card.id)}
+                    />
+                  </Box>
+                  <Typography
+                    sx={{
+                      color: "text.secondary",
+                      mb: 1.5,
+                    }}
+                  >
+                    {card.description}
+                  </Typography>
+                  <Stack direction="row" spacing={1} flexWrap="wrap" useFlexGap>
+                    <Chip
+                      label={card.priority}
+                      sx={{
+                        backgroundColor: "#e0f2f1",
+                        border: "1px solid #ccc",
+                        color: "#424242",
+                        fontWeight: "bold",
+                        fontSize: "13px",
+                        padding: "4px",
+                      }}
+                    />
+                    {card.tags.map((item, idx) => (
+                      <Chip
+                        key={idx}
+                        label={item}
+                        icon={<LocalOfferIcon sx={{ width: "13px" }} />}
+                        sx={{
+                          backgroundColor: "white",
+                          border: "1px solid #ccc",
+                          color: "#616161",
+                          fontWeight: "bold",
+                          fontSize: "11px",
+                        }}
+                      />
+                    ))}
+                  </Stack>
+                </CardContent>
+
+                <CardActions
+                  className="flex flex-col gap-4 "
+                  sx={{
+                    padding: "8px  16px 10px 16px",
+                    color: "#616161",
+                    alignItems: "start",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      gap: "40px",
+                      margin: 0,
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "start",
+                      }}
+                    >
+                      <Typography sx={{ fontSize: "16px" }}>updated</Typography>
+                      <Typography sx={{ fontSize: "14px" }}>
+                        {card.date}{" "}
+                      </Typography>
+                    </Box>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "space-around",
+                        gap: "10px",
+                      }}
+                    >
+                      <EditIcon
+                        onClick={() => ButtonOnclick("edit", card.id)}
+                      />
+                      <DeleteIcon
+                        onClick={() => ButtonOnclick("delete", card.id)}
+                      />
+                    </Box>
+                  </Box>
+                </CardActions>
+              </Card>
+            );
+          }
+        })}
       </Box>
     </Box>
   );
